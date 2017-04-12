8f5c653d50911d373db8d14f2609ddc975764bc1
baiyuexingchen@gmail.com <baiyuexingchen@gmail.com>
2017-04-12 22:17:31 +0800

    I love to record my trace and let it to be seen on google earth,but no app help me achieve this.

---
+++ app/src/main/java/com/example/message/earth/MainActivity.java
@@ -0,0 +1,16 @@
+package com.example.message.earth;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.EditText;
+
+public class MainActivity extends AppCompatActivity {
+    private EditText jingdu=null;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+    }
+}
